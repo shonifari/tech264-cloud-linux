c40a384fbfb4b4cd334ac14854738e7c267acca9
Karis Omotosho <karis.omotosho@gmail.com>
2024-10-25 09:30:21 +0100

    Edited mistake in script. File is 'seed.js' not 'seeds.js'

--- deploying-apps/run-app-only.sh
+++ deploying-apps/run-app-only.sh
@@ -3,13 +3,12 @@
 REPO="tech264-sparta-app"
 
 # Set DB_HOST variable 
-export DB_HOST="mongodb://10.0.4.4:27017/posts"
-export DB_HOST="mongodb://172.31.15.212:27017/posts"
+export DB_HOST="mongodb://10.0.3.4:27017/posts"
 
 cd /$REPO/app/
 
-node ./seeds/seeds.js
+node ./seeds/seed.js
 
 pm2 stop all
 
-pm2 start app.js
+pm2 start app.js 
